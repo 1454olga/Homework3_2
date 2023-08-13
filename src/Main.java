df42d0e6de5568b8e5dd03659041f73fc79c79d1
Olga <olga-kh.neru@yandex.ru>
2023-08-13 20:58:10 +0900

    сделано задание

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,35 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
+        task5();
     }
 
     public static void task1 () {
         System.out.println("Задача 1");
-        ... // Пишем код для задачи 1
+        int clienOS = 1;
+        if (clienOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }   else if (clienOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }   else {
+            System.out.println("неверное значение");
+        }
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
-        ... // Пишем код для задачи 2
+
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+    }
+
+    public static void task4 () {
+        System.out.println("Задача 4");
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
     }
 }
